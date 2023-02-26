57b5d69da6a374c26b82a0df2dac98c0524efe99
Jseongwon <48094527+Jseongwon@users.noreply.github.com>
2023-02-26 20:19:04 +0900

    second commit

--- Noticeboard/Data/Migrations/20230226110830_NoticeBoardInit.Designer.cs
+++ Noticeboard/Data/Migrations/20230226110830_NoticeBoardInit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Noticeboard.Data;
 
@@ -11,9 +12,11 @@ using Noticeboard.Data;
 namespace Noticeboard.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230226110830_NoticeBoardInit")]
+    partial class NoticeBoardInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Noticeboard/Data/Migrations/20230226110830_NoticeBoardInit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Noticeboard.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoticeBoardInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NoticeBoard",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreaterId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoticeBoard", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_NoticeBoard_AspNetUsers_CreaterId",
+                        column: x => x.CreaterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoticeBoard_CreaterId",
+                table: "NoticeBoard",
+                column: "CreaterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoticeBoard");
+        }
+    }
+}
